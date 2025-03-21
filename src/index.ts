9fe4f6b8fe5ba587671d48798eecedb296d4dc8b
vakharwalad23 <vakharwala.d@gmail.com>
2025-03-22 01:06:17 +0530

    feat: enhance tweet fetching with additional query parameters and headers

--- src/index.ts
+++ src/index.ts
@@ -436,10 +436,19 @@ export class Helpers {
 	}
 
 	async handleTweet(tweetId: string): Promise<Tweet | null> {
-		const url = `https://cdn.syndication.twimg.com/tweet-result?id=${tweetId}&lang=en`;
+		const url = `https://cdn.syndication.twimg.com/tweet-result?id=${tweetId}&lang=en&features=tfw_timeline_list%3A%3Btfw_follower_count_sunset%3Atrue%3Btfw_tweet_edit_backend%3Aon%3Btfw_refsrc_session%3Aon%3Btfw_fosnr_soft_interventions_enabled%3Aon%3Btfw_show_birdwatch_pivots_enabled%3Aon%3Btfw_show_business_verified_badge%3Aon%3Btfw_duplicate_scribes_to_settings%3Aon%3Btfw_use_profile_image_shape_enabled%3Aon%3Btfw_show_blue_verified_badge%3Aon%3Btfw_legacy_timeline_sunset%3Atrue%3Btfw_show_gov_verified_badge%3Aon%3Btfw_show_business_affiliate_badge%3Aon%3Btfw_tweet_edit_frontend%3Aon&token=4c2mmul6mnh`;
 		try {
 			const resp = await fetch(url, {
-				headers: { 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)' },
+				headers: {
+					'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)',
+					Accept: 'application/json',
+					'Accept-Language': 'en-US,en;q=0.5',
+					'Accept-Encoding': 'gzip, deflate, br',
+					Connection: 'keep-alive',
+					'Upgrade-Insecure-Requests': '1',
+					'Cache-Control': 'max-age=0',
+					TE: 'Trailers',
+				},
 			});
 			if (!resp.ok) throw new Error(`HTTP ${resp.status}`);
 			return (await resp.json()) as Tweet;
